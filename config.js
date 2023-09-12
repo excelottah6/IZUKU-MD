b01c2d421d438ef653992f4bde0df5e7ebe6296d
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-12 14:52:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'DEKU',
+  LANG: process.env.THEME|| 'IZUKU',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
 
