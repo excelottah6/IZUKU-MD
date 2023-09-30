961e54e01c617de66e39288a3fe5e6bf667a8c2b
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-30 12:10:26 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -17,18 +17,32 @@
  **/
 
 //---------------------------------------------------------------------------
-const Secktor = require('../lib')
+const Secktor = require('../lib');
 Secktor.cmd({
-        pattern: "ping",
-        desc: "To check ping",
-        category: "general",
-        filename: __filename,
-    },
-    async(Void, citel) => {
-        var inital = new Date().getTime();
-        const { key } = await Void.sendMessage(citel.chat, {text: '```Pinging from server```'});
-        var final = new Date().getTime();
-       // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
-    }
-);
+    pattern: "ping",
+    desc: "To check ping",
+    category: "general",
+    filename: __filename,
+},
+async (Void, citel) => {
+    var initial = new Date().getTime();
+    const { key } = await Void.sendMessage(citel.chat, { text: '```Pinging from server```' });
+    var final = new Date().getTime();
+
+    // Calculate the ping value in milliseconds
+    const pingValue = final - initial;
+
+    // Create a loading bar
+    const loadingBarLength = 20;
+    const loadedBlocks = loadingBarLength;
+    const loadingBar = '█'.repeat(loadedBlocks);
+
+    // Send the loading bar first
+    await Void.sendMessage(citel.chat, { text: `*Pong*\nLoading: [${loadingBar}] 100%`, edit: key });
+
+    // Wait for a brief moment (you can customize this delay)
+    await Secktor.sleep(1000);
+
+    // Send the final ping message
+    return await Void.sendMessage(citel.chat, { text: `*Ping: ${pingValue} ms*` });
+});
