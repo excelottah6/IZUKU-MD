b58468b7f12e7bfb9015d9b361d24fbf0bc97727
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-03 18:36:02 +0100

    Update setaza.js

--- commands/setaza.js
+++ commands/setaza.js
@@ -1,52 +1,46 @@
 const { cmd } = require('../lib');
 
-// Initialize an object to store recorded text
-const recordedText = {};
+const recordedText = {}; 
 
-// Command to record text
 cmd({
   pattern: "setaza",
   desc: "Record a text message",
   category: "utility",
 }, async (Void, citel, text) => {
   const recorded = text.trim(); // Get the recorded text from the command.
-  const userId = citel.sender; // Use the sender's ID as the key.
+  const senderId = citel.sender; // Use the sender's ID as the key.
 
-  recordedText[userId] = recorded; // Store the recorded text for this user.
+  recordedText[senderId] = recorded; // Store the recorded text for this user.
 
   await citel.reply(`aza has been recorded boss: "${recorded}"`);
 });
 
-// Command to delete recorded text
 cmd({
-  pattern: "delaza",
-  desc: "Delete the recorded text",
+  pattern: "send aza",
+  desc: "Send the recorded text",
   category: "utility",
 }, async (Void, citel) => {
-  const userId = citel.sender; // Get the sender's ID.
+  const senderId = citel.sender; // Get the sender's ID.
 
-  if (recordedText[userId]) {
-    delete recordedText[userId]; // Remove the recorded text for this user.
-    await citel.reply("Recorded text has been deleted.");
+  if (recordedText[senderId]) {
+    const recorded = recordedText[senderId]; // Get the recorded text for this user.
+    await citel.reply(`Here's the recorded text: "${recorded}"`);
   } else {
     await citel.reply("No recorded text found.");
   }
 });
 
-// Listen for incoming messages
 cmd({
-  on: "text",
-}, async (Void, citel, text) => {
-  // Check if the received message contains "aza" or "send aza" (case-insensitive).
-  if (/(\aza\b|\send aza\b)/i.test(text)) {
-    const senderId = citel.sender; // Get the sender's ID.
+  pattern: "deleteaza",
+  desc: "Delete the recorded text",
+  category: "utility",
+}, async (Void, citel) => {
+  const senderId = citel.sender; // Get the sender's ID.
 
-    // Check if there's recorded text for the sender.
-    if (recordedText[senderId]) {
-      const recorded = recordedText[senderId];
-
-      // Reply with the recorded text.
-      await citel.reply(recorded);
-    }
+  if (recordedText[senderId]) {
+    delete recordedText[senderId]; // Delete the recorded text for this user.
+    await citel.reply("Recorded text has been deleted.");
+  } else {
+    await citel.reply("No recorded text found.");
   }
 });
