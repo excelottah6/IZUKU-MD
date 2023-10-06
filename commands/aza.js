ac5e53110028f96d7b64af654d02433412c71eed
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-06 13:21:08 +0100

    Update aza.js

--- commands/aza.js
+++ commands/aza.js
@@ -1,50 +1,26 @@
-const { bot, cmd } = require('../lib');
+const { cmd } = require('../lib');
 
-// Initialize an object to store recorded text
-const recordedText = {};
+let recordedMessage = '';
 
-// Command to record text
 cmd({
   pattern: "setaza",
-  desc: "Record a text message",
+  desc: "Store a message as account number",
   category: "utility",
 }, async (Void, citel, text) => {
-  const recorded = text.trim(); // Get the recorded text from the command.
-  const userId = citel.sender; // Use the sender's ID as the key.
-
-  recordedText[userId] = recorded; // Store the recorded text for this user.
-
-  await citel.reply(`aza has been recorded boss: "${recorded}"`);
+  const message = text.trim();
+  recordedMessage = message;
+  await citel.reply(`Account number recorded: "${message}"`);
 });
 
-// Command to delete recorded text
 cmd({
-  pattern: "delaza",
-  desc: "Delete the recorded text",
-  category: "utility",
-}, async (Void, citel) => {
-  const userId = citel.sender; // Get the sender's ID.
-
-  if (recordedText[userId]) {
-    delete recordedText[userId]; // Remove the recorded text for this user.
-    await citel.reply("Recorded text has been deleted.");
-  } else {
-    await citel.reply("No recorded text found.");
-  }
-});
-
-// Listen for incoming messages
-bot.on('message', async (message) => {
-  const userId = message.sender; // Get the sender's ID
-  const text = message.content.toLowerCase(); // Convert the message content to lowercase for case-insensitive matching
-
-  if (text.includes("send aza")) {
-    const recorded = recordedText[userId]; // Get the recorded text for this user
-
-    if (recorded) {
-      await bot.sendMessage(userId, `Here's our account number: ${recorded}`); // Send the recorded text to the user
+  on: "text",
+}, async (Void, citel, text) => {
+  if (/(\bsend aza\b)/i.test(text)) {
+    const sender = citel.sender;
+    if (recordedMessage !== '') {
+      await citel.sendMessage(sender, recordedMessage);
     } else {
-      await bot.sendMessage(userId, "No recorded text found."); // Send a message indicating no recorded text is available
+      await citel.reply("No account number recorded.");
     }
   }
 });
