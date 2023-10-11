5c50c3e72f4a67694cedd88c6cf915b5dda61b98
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-11 17:54:37 +0100

    Update aza.js

--- commands/aza.js
+++ commands/aza.js
@@ -1,36 +1,57 @@
 const { cmd } = require('../lib');
 
 let recordedMessage = '';
+let owner = ''; // Initialize owner as an empty string
 
 cmd({
-  pattern: "setaza",
-  desc: "Store a message as account number",
+  pattern: "setowner (.+)",
+  desc: "Set the bot owner",
   category: "utility",
-  fromMe: true, // Added "fromMe" to make it private
-}, async (Void, citel, text) => {
-  const message = text.trim();
-  recordedMessage = message;
-  await citel.reply(`Account number recorded: "${message}"`);
+  fromMe: true, // Make it a private command for the owner
+}, async (Void, citel, match) => {
+  if (citel.sender === owner) {
+    owner = match[1].trim(); // Set the owner when they use this command
+    await citel.reply(`Owner set as: "${owner}"`);
+  } else {
+    await citel.reply("You don't have permission to set the owner.");
+  }
 });
 
 cmd({
-  pattern: "delaza", // Added command to delete recorded message
+  pattern: "setaza (.+)",
+  desc: "Store a message as account number",
+  category: "utility",
+  fromMe: true, // Make it a private command for the owner
+}, async (Void, citel, match) => {
+  if (citel.sender === owner) {
+    const message = match[1].trim();
+    recordedMessage = message;
+    await citel.reply(`Account number recorded: "${message}"`);
+  } else {
+    await citel.reply("You don't have permission to set the account number.");
+  }
+});
+
+cmd({
+  pattern: "delaza",
   desc: "Delete the recorded account number",
   category: "utility",
-  fromMe: true, // Make it a private command
+  fromMe: true, // Make it a private command for the owner
 }, async (Void, citel) => {
-  recordedMessage = ''; // Delete the recorded message
-  await citel.reply("Account number deleted.");
+  if (citel.sender === owner) {
+    recordedMessage = '';
+    await citel.reply("Account number deleted.");
+  } else {
+    await citel.reply("You don't have permission to delete the account number.");
+  }
 });
 
 cmd({
   on: "text",
 }, async (Void, citel, text) => {
-  // Updated the regular expression to match "send aza" case-insensitively
-  if (/send aza/i.test(text)) {
-    const sender = citel.sender;
-    if (recordedMessage !== '') {
-      await citel.sendMessage(sender, recordedMessage);
+  if (text && text.toLowerCase().includes("send aza")) {
+    if (recordedMessage) {
+      await citel.reply(recordedMessage);
     } else {
       await citel.reply("No account number recorded.");
     }
