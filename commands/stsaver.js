d5fcdf9f3fc9f94549f498683991655ca1c84d03
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-14 00:36:47 +0100

    Update stsaver.js

--- commands/stsaver.js
+++ commands/stsaver.js
@@ -1,51 +1,23 @@
-const { cmd, owner } = require('../lib');
-let recordedText = "";
-let recordedAudio = "";
-let recordedVideo = "";
-
 cmd({
   pattern: "save",
-  desc: "Save text, audio, and video files",
-  category: "utility",
-}, async (Void, citel, text) => {
-  if (citel.from === owner) {
-    const chatId = citel.chatId;
-    const chatDir = `./chats/${chatId}`;
-
-    // Create the chat directory if it doesn't exist
-    if (!fs.existsSync(chatDir)) {
-      fs.mkdirSync(chatDir);
-    }
-
-    const textFile = `${chatDir}/text.txt`;
-    const audioFile = `${chatDir}/audio.mp3`;
-    const videoFile = `${chatDir}/video.mp4`;
-
-    // Save text file
-    fs.writeFileSync(textFile, recordedText);
-
-    // Save audio file
-    const audioBuf = await getAudioBuffer(recordedAudio);
-    fs.writeFileSync(audioFile, audioBuf);
-
-    // Save video file
-    const videoBuf = await getVideoBuffer(recordedVideo);
-    fs.writeFileSync(videoFile, videoBuf);
-
-    await citel.reply(`Saved text, audio, and video files to chat ${chatId}`);
-  } else {
-    await citel.reply("Sorry, only the owner can save files.");
-  }
+  desc: 'Save WhatsApp status to log number',
+  category: 'whatsapp',
+  filename: __filename,
+},
+async (msg, text, { cmdName, isCreator }) => {
+  if (!isCreator) return await msg.send(tlang().owner);
+  if (!msg.quoted) return await msg.send("*hey Please, reply to a Message*");
+  const status = await msg.quoted.getStatus();
+  const logNumber = await getLogNumber();
+  const date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+  const message = `Status saved to log number ${logNumber} on ${date}`;
+  await msg.send(message);
+  await saveStatus(status, logNumber);
 });
 
-async function getAudioBuffer(audio) {
-  // Convert audio to buffer
-  const audioBuf = await audio.toPromise();
-  return audioBuf;
+function getLogNumber() {
+  return logNumber;
 }
 
-async function getVideoBuffer(video) {
-  // Convert video to buffer
-  const videoBuf = await video.toPromise();
-  return videoBuf;
+function saveStatus(status, logNumber) {
 }
