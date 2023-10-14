23e1410993e22033e20ee2a5aa7255985eb9b146
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-14 22:06:44 +0100

    Update aza.js

--- commands/aza.js
+++ commands/aza.js
@@ -25,37 +25,12 @@ cmd({
   recordedMessage = '';
   await citel.reply("Account number deleted.");
 });
-
+//=======================================++++================
 cmd({
   on: "text",
 }, async (Void, citel, text) => {
-  if (/send aza/i.test(text)) {
-    const sender = citel.sender;
-    if (recordedMessage !== '') {
-      await citel.sendMessage(sender, recordedMessage);
-    }
+  if (/(\baza\b|\bsend aza\b)/i.test(text) && recordedMessage) {
+    await citel.reply(recordedMessage);
   }
 });
-//--------------------------------------------------------------
-cmd({
-  pattern: "save",
-  desc: "Save WhatsApp status to log number",
-  category: "utility",
-},
-async (msg, text, { cmdName, isCreator }) => {
-  if (!isCreator) return await msg.send(tlang().owner);
-  if (!msg.quoted) return await msg.send("*hey Please, reply to a Message*");
-  const status = await msg.quoted.getStatus();
-  const logNumber = await getLogNumber();
-  const date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
-  const message = `Status saved to log number ${logNumber} on ${date}`;
-  await msg.send(message);
-  await saveStatus(status, logNumber);
-});
 
-function getLogNumber() {
-  return logNumber;
-}
-
-function saveStatus(status, logNumber) {
-}
