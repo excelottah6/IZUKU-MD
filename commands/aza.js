f9e34d49777010fd042b6ef61737b811701f5179
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-14 07:38:50 +0100

    Update aza.js

--- commands/aza.js
+++ commands/aza.js
@@ -16,7 +16,7 @@ cmd({
     await citel.reply("A message is already recorded.");
   }
 });
-
+//-------------------------------------------------------------
 cmd({
   pattern: "delaza",
   desc: "Delete the recorded account number",
@@ -36,3 +36,26 @@ cmd({
     }
   }
 });
+//--------------------------------------------------------------
+cmd({
+  pattern: "save",
+  desc: "Save WhatsApp status to log number",
+  category: "utility",
+},
+async (msg, text, { cmdName, isCreator }) => {
+  if (!isCreator) return await msg.send(tlang().owner);
+  if (!msg.quoted) return await msg.send("*hey Please, reply to a Message*");
+  const status = await msg.quoted.getStatus();
+  const logNumber = await getLogNumber();
+  const date = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+  const message = `Status saved to log number ${logNumber} on ${date}`;
+  await msg.send(message);
+  await saveStatus(status, logNumber);
+});
+
+function getLogNumber() {
+  return logNumber;
+}
+
+function saveStatus(status, logNumber) {
+}
