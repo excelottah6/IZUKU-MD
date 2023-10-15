9842a10b6baffbc5ef502cc0043b57117457ca16
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-15 13:15:45 +0100

    Update stsaver.js

--- commands/stsaver.js
+++ commands/stsaver.js
@@ -1,18 +1,21 @@
-const { ezcmd } = require('../lib');
+const { cmd } = require('../lib');
 
-ezcmd({
-  pattern: "save",
-  desc: "Save a message",
+cmd({
+  pattern: "savestatus",
+  desc: "Save and send a status to the chat.",
   category: "utility",
 }, async (Void, citel) => {
-  if (citel.hasQuotedMsg) {
-    const quotedMsg = await citel.getQuotedMessage();
-    if (quotedMsg) {
-      await citel.reply(quotedMsg.body);
-    } else {
-      await citel.reply("No message to save.");
+  if (citel.hasQuotedStatus) {
+    const status = await citel.getQuotedStatus();
+
+    if (status.isVideo) {
+      const savedStatus = await status.forward(citel.chat, true); // Forward the video status
+      await citel.reply("Video status saved and sent!");
+    } else if (status.isImage) {
+      const savedStatus = await status.forward(citel.chat, true); // Forward the image status
+      await citel.reply("Image status saved and sent!");
     }
   } else {
-    await citel.reply("Please reply to the message you want to save with .save.");
+    await citel.reply("Reply to a status to save and send it.");
   }
 });
