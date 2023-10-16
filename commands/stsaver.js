eb8b253da1a6b0e7951d6fc45118e96fd8267311
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-16 06:18:06 +0100

    Update stsaver.js

--- commands/stsaver.js
+++ commands/stsaver.js
@@ -4,19 +4,18 @@ cmd({
   pattern: "save",
   desc: "Save status to the chat.",
   category: "misc",
-  filename: __filename,
-  fromMe: true, // Ensure it's a private command
-  onlyGroup: false, // Allow using it outside of groups
+  fromMe: true, 
 }, async (Void, citel, match) => {
   if (citel.hasQuoted) {
     const quotedMessage = await citel.getQuotedMessage();
 
     if (quotedMessage.isStatus) {
       await citel.send(quotedMessage);
-    } else {
-      await citel
+    }
+  }
+});
 
-    cmd({
+cmd({
   on: "text",
 }, async (Void, citel, text) => {
   if (citel.quoted && text.toLowerCase().includes("send")) {
