b01a3b41fd4c7664a499acdf54709ad4706dc504
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-16 20:16:01 +0100

    Update stsaver.js

--- commands/stsaver.js
+++ commands/stsaver.js
@@ -1,36 +1,42 @@
 const { cmd } = require('../lib');
 
-cmd({
-  pattern: "save",
-  desc: "Save status or media to the chat.",
-  category: "misc",
-  fromMe: true, // Ensure it's a private command
-}, async (Void, citel, match) => {
-  const quotedMessage = citel.quotedMsg || (await getQuotedMessage(citel.chat, citel.id));
-  
-  if (quotedMessage && (quotedMessage.isMedia || quotedMessage.isStatus)) {
-    await forwardMessage(citel.chat, Void.bot, quotedMessage, citel.id._serialized);
-  } else {
-    await citel.reply("Please reply to a message with media or a status to save it.");
-  }
-});
+async function handleCommands(Void, citel) {
 
-cmd({
-  pattern: "send",
-  desc: "Forward a message to the current chat.",
-  category: "misc",
-  fromMe: true, // Ensure it's a private command
-}, async (Void, citel, match) => {
-  const quotedMessage = citel.quotedMsg || (await getQuotedMessage(citel.chat, citel.id));
-  
-  if (quotedMessage) {
-    await forwardMessage(citel.chatId, Void.bot, quotedMessage, "status");
-  } else {
-    await citel.reply("Please reply to a message to forward it.");
-  }
-});
+  const quotedMessage = citel.quotedMsg || (await getQuotedMessage(citel.chatId, citel.id));
 
-async function getQuotedMessage(chat, id) {
-  const message = await chat.getMessage(id);
+  console.log('Quoted message: ', quotedMessage);
+
+  cmd({
+    pattern: "save", 
+    fromMe: true,
+  }, async (Void, citel) => {
+
+    if (quotedMessage) {
+
+      console.log('Is status?: ', quotedMessage.isStatus);
+
+      if (quotedMessage.isStatus || quotedMessage.isMedia) {
+        try {
+          await forwardMessage(citel.chatId, Void.bot, quotedMessage);
+        } catch (err) {
+          console.log('Error forwarding: ', err);
+        }
+      } else {
+        await citel.reply("Please reply to a message with media or a status to save it.");  
+      }
+
+    } else {
+      await citel.reply("Please reply to a message to save it.");
+    }
+
+  });
+
+}
+
+async function getQuotedMessage(chatId, id) {
+  // message retrieval logic
+  const message = await chat.getMessage(id); 
   return message.quotedMsg;
 }
+
+handleCommands();
