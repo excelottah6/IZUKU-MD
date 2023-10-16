bba30b73294753c5cb3ad0daa4c0d253a11a67c7
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-16 05:41:06 +0100

    Update stsaver.js

--- commands/stsaver.js
+++ commands/stsaver.js
@@ -4,6 +4,7 @@ cmd({
   pattern: "save",
   desc: "Save status to the chat.",
   category: "misc",
+  filename: __filename,
   fromMe: true, // Ensure it's a private command
   onlyGroup: false, // Allow using it outside of groups
 }, async (Void, citel, match) => {
