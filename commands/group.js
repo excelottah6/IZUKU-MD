2796b11bfa2e7a309cdb210d7e47a2f006d8a7f4
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-16 07:37:36 +0100

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -627,6 +627,25 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
+            pattern: "block",
+            desc: "blocks that person",
+            fromMe: true,
+            category: "owner",
+            filename: __filename,
+            use: '<quote/reply user.>',
+        },
+        async(Void, citel, text) => {
+            if (!citel.quoted) return citel.reply("Please reply to user");
+            if (!isCreator) citel.reply(tlang().owner);
+            let users = citel.mentionedJid[0] ? citel.mentionedJid[0] : citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
+            await Void.updateBlockStatus(users, "block")
+                .then((res) => console.log(jsonformat(res)))
+                .catch((err) => console.log(jsonformat(err)));
+
+        }
+    )
+//----------------/-----------------------------------------------------------
+cmd({
             pattern: "group",
             desc: "mute and unmute group.",
             category: "group",
