5d42193aedee4f0a916ee3d7b4ac7d9af172a18c
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-17 03:23:00 +0100

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -108,28 +108,28 @@ cmd({
    pattern: "setgreeting",
    desc: "Set a custom greeting message",
    category: "group",
- }, async (Void, citel, text) => {
-   const greeting = text.trim(); // Get the custom greeting message from the command.
-   const userId = citel.sender; // Use the sender's ID as the key.
+ }, async (message, match) => {
+   const greeting = match[1].trim(); // Get the custom greeting message from the command.
+   const userId = message.sender.id; // Use the sender's ID as the key.
 
    greetingMessages[userId] = greeting; // Store the custom greeting message for this user.
 
-   await citel.reply("Custom greeting message set successfully!");
+   await message.reply("Custom greeting message set successfully!");
  });
 
  // Listen for incoming messages
- bot.on('message', async (message) => {
-   const userId = message.sender; // Get the sender's ID
+text.on('message', async (message) => {
+   const userId = message.sender.id; // Get the sender's ID
 
    // Check if the sender has a custom greeting message
    if (greetingMessages[userId]) {
      const greeting = greetingMessages[userId]; // Get the custom greeting message for this user
-     await bot.sendMessage(userId, `👋 ${greeting}`); // Send the custom greeting message to the user
+     await message.sendMessage(userId, `👋 ${greeting}`); // Send the custom greeting message to the user
    } else {
-     await bot.sendMessage(userId, "👋 Hello! How can I assist you?"); // Send a default greeting message if no custom greeting is set
+     await message.sendMessage(userId, "👋 Hello! How can I assist you?"); // Send a default greeting message if no custom greeting is set
    }
- });
-//-----------——•-----------------------------------------111-1-11-1
+});
+//-----------——•--------------------------------------------------
 cmd({
             pattern: "hidetag",
             alias: ["htag"],
@@ -955,54 +955,47 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
-        pattern: "broadcast",
-        alias: ["bc"],
-        desc: "Bot makes a broadcast in all groups",
-        fromMe: true,
-        category: "group",
-        filename: __filename,
-        use: '<text for broadcast.>',
-    },
-    async(Void, citel, text) => {
-        if (!isCreator) return citel.reply(tlang().owner)
-        let getGroups = await Void.groupFetchAllParticipating();
-        let groups = Object.entries(getGroups)
-            .slice(0)
-            .map((entry) => entry[1]);
-        let anu = groups.map((v) => v.id);
-        citel.reply(`Send Broadcast To ${anu.length} Group Chat, Finish Time ${
-          anu.length * 1.5
-        } second`);
-        for (let i of anu) {
-            await sleep(1500);
-            let txt = `*--❗${tlang().title} Broadcast❗--*\n\n *👾Author:* ${citel.pushName}\n\n${text}`;
-            let buttonMessaged = {
-                image: log0,
-                caption: txt,
-                footer: citel.pushName,
-                headerType: 1,
-                contextInfo: {
-                    forwardingScore: 999,
-                    isForwarded: false,
-                    externalAdReply: {
-                        title: 'Broadcast by ' + citel.pushName,
-                        body: tlang().title,
-                        thumbnail: log0,
-                        mediaUrl: '',
-                        mediaType: 2,
-                        sourceUrl: gurl,
-                        showAdAttribution: true,
-                    },
+    pattern: "broadcast",
+    alias: ["bc"],
+    desc: "Bot makes a broadcast in all groups",
+    fromMe: true,
+    category: "group",
+    filename: __filename,
+    usage: "<text for broadcast>",
+}, async (message, match) => {
+    if (!isCreator) return message.reply(tlang().owner);
+    let getGroups = await message.groupFetchAllParticipating();
+    let groups = Object.entries(getGroups).map((entry) => entry[1]);
+    let anu = groups.map((v) => v.id);
+    message.reply(`Sending Broadcast to ${anu.length} Group Chat(s). Estimated Time: ${anu.length * 1.5} seconds.`);
+    for (let i of anu) {
+        await sleep(1500);
+        let txt = `*--❗ ${tlang().title} Broadcast ❗--*\n\n👾 Author: ${message.pushName}\n\n${match[1]}`;
+        let buttonMessage = {
+            image: log0,
+            caption: txt,
+            footer: message.pushName,
+            headerType: 1,
+            contextInfo: {
+                forwardingScore: 999,
+                isForwarded: false,
+                externalAdReply: {
+                    title: 'Broadcast by ' + message.pushName,
+                    body: tlang().title,
+                    thumbnail: log0,
+                    mediaUrl: '',
+                    mediaType: 2,
+                    sourceUrl: gurl,
+                    showAdAttribution: true,
                 },
-            };
-            await Void.sendMessage(i, buttonMessaged, {
-                quoted: citel,
-            });
-        }
-        citel.reply(`*Successful Sent Broadcast To ${anu.length} Group(s)*`);
+            },
+        };
+        await message.sendMessage(i, buttonMessage, {
+            quoted: message,
+        });
     }
-)
-
+    message.reply(`Successfully Sent Broadcast to ${anu.length} Group(s).`);
+});
 //---------------------------------------------------------------------------
 if(Config.WORKTYPE!=='private'){
 cmd({ on: "text" }, async(Void, citel) => {
