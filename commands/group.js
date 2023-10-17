f3996585f323c765083e6931a424b3d5853513fa
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-17 09:26:34 +0100

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -714,6 +714,32 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
+            pattern: "tag",
+            alias: ["tag"],
+            desc: "Tags everyperson of group without mentioning their numbers",
+            category: "group",
+            filename: __filename,
+            use: '<text>',
+        },
+        async(Void, citel, text) => {
+            if (!citel.isGroup) return citel.reply(tlang().group);
+            const groupMetadata = citel.isGroup ? await Void.groupMetadata(citel.chat).catch((e) => {}) : "";
+            const participants = citel.isGroup ? await groupMetadata.participants : "";
+            const groupAdmins = await getAdmin(Void, citel)
+            const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
+            if (!isAdmins) return citel.reply(tlang().admin);
+
+            if (!isAdmins) citel.reply(tlang().admin);
+            Void.sendMessage(citel.chat, {
+                text: text ? text : "",
+                mentions: participants.map((a) => a.id),
+            }, {
+                quoted: citel,
+            });
+        }
+    )
+//-----------------------------------------------------------------
+cmd({
             pattern: "add",
             desc: "Add that person in group",
             fromMe: true,
