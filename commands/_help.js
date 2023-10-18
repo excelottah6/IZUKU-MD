6536fdda44cefdd2f2b14cd6257618a3a4ec25ab
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-18 02:24:43 +0100

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -77,14 +77,14 @@ Secktor.cmd({
 ` + '```'
                 for (const category in cmds) 
                 {
-                   str += `◎═════•『*${tiny(category)}*』•═════◎\n` ;
+                   str += `◎═══•『*${tiny(category)}*』•═══◎\n` ;
                    if(text.toLowerCase() == category.toLowerCase()){ str = `┏━━━•❃ *${tiny(category)}* ❃•━━━┓\n` ;      
                         for (const plugins of cmds[category]) { str += `「★」${fancytext(plugins,1)}\n` ; }
-                        str += `──── ✧《✩》✧ ────\n`  ;
+                        str += `─── ✧《✩》✧ ───\n`  ;
                         break ;
                    }
                    else { for (const plugins of cmds[category]) { str += `「★」${fancytext(plugins,1)}\n` ; }
-                         str += `──── ✧《✩》✧ ────\n`  ; 
+                         str += `─── ✧《✩》✧ ───\n`  ; 
                    }
   
                 }
