4765516be75d88e99c78ab2fbbbad08c41fb15d1
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-20 07:22:09 +0100

    Update Watsusi.js

--- commands/Watsusi.js
+++ commands/Watsusi.js
@@ -1,22 +1,17 @@
+const { cmd, Void, Config } = require("../lib");
 const moment = require("moment-timezone");
-const Config = require('../config');
-const { fancytext, tlang, tiny, botpic, prefix, cmd } = require("../lib");
-const { forwardMessage } = require("../lib");
 
 cmd({
   pattern: "save",
-  desc: "Save WhatsApp message or status",
+  desc: "Save WhatsApp status",
   category: "whatsapp",
   filename: __filename,
 }, async (Void, citel) => {
-  if (citel.hasQuoted) {
-    const quotedJid = citel.getQuotedJid();
-    if (quotedJid) {
-      await Void.forwardMessages(citel.chat, [quotedJid], false);
-    } else {
-      await citel.reply("*Unable to retrieve the status or message jid*");
-    }
+  if (!citel.hasQuoted) return await citel.reply("*Please, reply to a WhatsApp status to save it*");
+  const quotedJid = citel.getQuotedJid();
+  if (quotedJid) {
+    await Void.forwardMessages(quotedJid, [citel.chat], false);
   } else {
-    await Void.forwardMessages(citel.chat, [citel.message], false);
+    await citel.reply("*Unable to retrieve the status jid*");
   }
 });
