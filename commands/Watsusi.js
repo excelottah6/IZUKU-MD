03b06f01ea572520c9b9d9959f1002a8ae933e53
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-20 08:49:07 +0100

    Update Watsusi.js

--- commands/Watsusi.js
+++ commands/Watsusi.js
@@ -1,24 +1,23 @@
-const { cmd } = require("../lib");
+const { cmd } = require('../lib');
 
 cmd({
   pattern: "save",
-  fromMe: true, 
-  desc: "Save WhatsApp status",
+  fromMe: true,
+  desc: "Forward replied message to yourself",
   category: "watsusi",
-  filename: __filename,
 }, async (Void, citel, text) => {
-  const contact = citel.sender; 
-  const status = await Void.client.StatusMessage(contact, 'recent');
-  if (status) {
-    if (status.type === 'image' || status.type === 'video') {
-      await Void.sendMessage(citel.sender, status);  
-      await citel.reply("Status saved successfully!");
-    } else if (status.type === 'text') {
-      await citel.reply(status.text); 
-    } else {
-      await citel.reply("No recent image, video, or text status found to save.");
-    }
+  if (!citel.hasQuoted) return await citel.reply("_Reply to a message_");
+
+  const quotedMessage = await citel.getQuotedMessage();
+
+  if (quotedMessage) {
+    await Void.sendMessage(Void.user.jid, { ...quotedMessage }, {
+      contextInfo: {
+        isForwarded: false,
+      },
+    });
+    return await citel.reply("*Message saved successfully*");
   } else {
-    await citel.reply("No recent status found to save.");
+    await citel.reply("*Unable to retrieve the message*");
   }
 });
