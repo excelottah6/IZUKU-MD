7e8e0fe9b57ad6e75744dde4359d521cc7af88d5
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-20 08:16:11 +0100

    Update Watsusi.js

--- commands/Watsusi.js
+++ commands/Watsusi.js
@@ -1,23 +1,15 @@
-const { cmd } = require('../lib');
+const { cmd,parsedJid } = require("../lib/");
 
 cmd({
-  pattern: "save",
-  fromMe: true,
-  desc: "Forward replied message to yourself",
-  category: "whatsapp",
-}, async (Void, citel, text) => {
-  if (!citel.hasQuoted) return await citel.reply("_Reply to a message_");
-
-  const quotedMessage = await citel.getQuotedMessage();
-
-  if (quotedMessage) {
-    await Void.forwardMessage(Void.user.jid, quotedMessage, {
-      contextInfo: {
-        isForwarded: false,
-      },
-    });
-    return await citel.reply("*Status saved successfully*");
-  } else {
-    await citel.reply("*Unable to retrieve the message*");
-  }
-});
+	pattern: 'save',
+	fromMe: true,
+	desc: 'forward replied msg to you',
+	type: 'watsusi'
+}, async (message, match) => {
+	if (!message.reply_message) return await message.reply('_Reply to a message_')
+	await message.client.forwardMessage(message.client.user.id, m.quoted_message, {
+		contextInfo: {
+			isForwarded: false
+		}
+	})
+})
