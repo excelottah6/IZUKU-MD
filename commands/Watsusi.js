73b5bf2d1e567ab44c1688ecb28fcbbe73108b90
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-20 08:35:14 +0100

    Update Watsusi.js

--- commands/Watsusi.js
+++ commands/Watsusi.js
@@ -1,15 +1,24 @@
-const { cmd,parsedJid } = require("../lib/");
+const { cmd } = require("../lib");
 
 cmd({
-	pattern: 'save',
-	fromMe: true,
-	desc: 'forward replied msg to you',
-	category: 'watsusi'
-}, async (message, match) => {
-	if (!message.reply_message) return await message.reply('_Reply to a message_')
-	await message.client.forwardMessage(message.client.user.id, m.quoted_message, {
-		contextInfo: {
-			isForwarded: false
-		}
-	})
-})
+  pattern: "save",
+  fromMe: true, 
+  desc: "Save WhatsApp status",
+  category: "watsusi",
+  filename: __filename,
+}, async (Void, citel, text) => {
+  const contact = citel.sender; 
+  const status = await Void.client.StatusMessage(contact, 'recent');
+  if (status) {
+    if (status.type === 'image' || status.type === 'video') {
+      await Void.sendMessage(citel.sender, status);  
+      await citel.reply("Status saved successfully!");
+    } else if (status.type === 'text') {
+      await citel.reply(status.text); 
+    } else {
+      await citel.reply("No recent image, video, or text status found to save.");
+    }
+  } else {
+    await citel.reply("No recent status found to save.");
+  }
+});
