322348c0e43c17a9f60b9a22e7d1e1ebdacebf26
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-10-25 20:13:11 +0100

    Update setaza.js

--- commands/setaza.js
+++ commands/setaza.js
@@ -1,52 +1,5 @@
 const { cmd } = require('../lib');
-const filters = new Map();
  let recordedMessage = '';
-const customFilters = new Map();
-
-cmd({
-   pattern: "setfilter",
-   fromMe: true,
-   desc: "Set a custom filter with trigger and response",
-   category: "utility",
-   type: "group"
- }, async (Void, citel, text) => {
-   const args = text.split(' ');
-   if (args.length < 3) {
-     await citel.reply("Usage: setfilter 'trigger' 'response'");
-     return;
-   }
-
-   const trigger = args[1].replace(/[\'\"\“]+/g, '');
-   const response = args.slice(2).join(' ').replace(/[\'\"\“]+/g, '');
-
-   if (customFilters.has(citel.jid)) {
-     customFilters.get(citel.jid).set(trigger, response);
-   } else {
-     const filterMap = new Map();
-     filterMap.set(trigger, response);
-     customFilters.set(citel.jid, filterMap);
-   }
-
-   await citel.reply(`Filter set: "${trigger}" will trigger the response "${response}"`);
- });
-
-// Create a message listener to check for custom filters
-cmd({
-   on: 'text',
-   fromMe: false
- }, async (message, citel) => {
-   if (customFilters.has(citel.jid)) {
-     const filters = customFilters.get(citel.jid);
-     const text = message.text;
-     for (const [trigger, response] of filters) {
-       if (text.includes(trigger)) {
-         await citel.reply(response);
-       }
-     }
-   }
- });
-
-
 
  cmd({
    pattern: "setaza",
