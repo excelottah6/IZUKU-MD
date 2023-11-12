f65fdfc7f6007a44a82b7f6e121b6a561d75c2b9
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-11-12 11:18:17 +0100

    Update tik.js

--- commands/tik.js
+++ commands/tik.js
@@ -1,10 +1,11 @@
-const TikTokScraper = require('tiktok-scraper');
 const { cmd } = require('../lib');
+const axios = require('axios');
+
 cmd({
   pattern: 'tiktok',
   fromMe: true,
-  catergory: 'downloader',
   desc: 'Download TikTok video without watermark',
+  catergory:'downloader',
 },
 async (Void, citel, text) => {
   const url = text.split(' ')[1];
@@ -13,21 +14,16 @@ async (Void, citel, text) => {
     return citel.reply('Please provide a TikTok video URL.');
   }
 
-  try {
-    const videoMeta = await TikTokScraper.getVideoMeta(url);
-    
-    if (videoMeta) {
-      // Check if the video has a noWatermark URL
-      const downloadUrl = videoMeta.collector[0].videoUrlNoWaterMark || videoMeta.collector[0].videoUrl;
+  const apiUrl = `https://vihangayt.me/download/tiktok?url=${encodeURIComponent(url)}`;
 
-      if (downloadUrl) {
-        // You can handle the download URL as needed, for example, send it as a reply
-        citel.reply(`Download URL: ${downloadUrl}`);
-      } else {
-        citel.reply('No download URL found. The video may be private or have restrictions.');
-      }
+  try {
+    const response = await axios.get(apiUrl);
+
+    if (response.data && response.data.video_url) {
+      // You can handle the download URL as needed, for example, send it as a reply
+      citel.reply(`Download URL: ${response.data.video_url}`);
     } else {
-      citel.reply('Unable to get information for the provided TikTok URL.');
+      citel.reply('Unable to fetch the download URL. The TikTok link may be invalid or the service is down.');
     }
   } catch (error) {
     citel.reply(`Error: ${error.message}`);
