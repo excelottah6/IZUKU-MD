3174e5c217b9bf29c6b24b883cf016bab7a61a7b
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-11-12 14:52:34 +0100

    Update misc.js

--- commands/misc.js
+++ commands/misc.js
@@ -453,7 +453,39 @@ let buttons = [{
 }
 }
 })   
-         
+//-------------------------------------------------111
+cmd({
+  pattern: 'tiktok',
+  fromMe: true,
+  desc: 'Download TikTok video without watermark',
+},
+async (Void, citel, text) => {
+  const url = text.split(' ')[1];
+
+  if (!url) {
+    return citel.reply('Please provide a TikTok video URL.');
+  }
+
+  const apiUrl = `https://vihangayt.me/download/tiktok?url=${encodeURIComponent(url)}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+
+    if (response.data && response.data.video_url) {
+      // Download the video
+      const videoResponse = await axios.get(response.data.video_url, { responseType: 'arraybuffer' });
+
+      // Send the video as a reply
+      await Void.sendMessage(citel.chat, { video: videoResponse.data }, MessageType.video, { quoted: citel.data });
+    } else {
+      citel.reply('Unable to fetch the download URL. The TikTok link may be invalid or the service is down.');
+    }
+  } catch (error) {
+    citel.reply(`Error: ${error.message}`);
+  }
+});
+
+
      //---------------------------------------------------------------------------
  cmd({
              pattern: "antilink",
