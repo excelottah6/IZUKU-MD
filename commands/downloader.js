8d0bc2a9307670a7a5959795899dd27a0d989041
ɪɴɴᴏxᴇɴᴛ-ᴢᴜʙɪ <143280418+Maher-Zubair@users.noreply.github.com>
2023-11-19 22:31:19 +0500

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -7,7 +7,7 @@
  * @author : SamPandey001 <https://github.com/SamPandey001>
  * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
- **/
+ **/ 
 
 const { tlang, ringtone, cmd,fetchJson, sleep, botpic,ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib')
 const { mediafire } = require("../lib/mediafire.js");
@@ -157,7 +157,7 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
-            pattern: "apk1",
+            pattern: "apk",
             desc: "Downloads apks  .",
             category: "downloader",
             filename: __filename,
@@ -165,20 +165,20 @@ cmd({
         },
 
         async(Void, citel, text) => {
-        if(!text )return citel.reply("*_Give me App Name_*");
+        if(!text )return citel.reply("*Give me App Name*");
 
 	const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`; };
 	let randomName = getRandom(".apk");
-	const filePath = `./temp/${randomName}`;     // fs.createWriteStream(`./${randomName}`)
+	const filePath = `./${randomName}`;     // fs.createWriteStream(`./${randomName}`)
         const {  search , download } = require('aptoide-scraper')
 	let searc = await search(text);          //console.log(searc);
 	let data={};
 	if(searc.length){ data = await download(searc[0].id); }
-	else return citel.reply("*_APP not Found, Try Other Name_*");
+	else return citel.send("*APP not Found, Try Other Name*");
 	
 	
 	const apkSize = parseInt(data.size);
-	if(apkSize > 100) return citel.reply(`❌ File size bigger than 150mb.`);
+	if(apkSize > 150) return citel.send(`❌ File size bigger than 200mb.`);
        const url = data.dllink;
 	 let  inf  ="*App Name :* " +data.name;
          inf +="\n*App id        :* " +data.package;
@@ -192,17 +192,33 @@ axios.get(url, { responseType: 'stream' })
   .then(response => {
     const writer = fs.createWriteStream(filePath);
     response.data.pipe(writer);
-    return new Promise((resolve, reject) => {writer.on('finish', resolve);writer.on('error', reject);});
-  }).then(() => {	
-	let buttonMessage = {document: fs.readFileSync(filePath),mimetype: 'application/vnd.android.package-archive',fileName: data.name+`.apk`,caption : inf}
-    Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
-    console.log('Apk File downloaded successfully');  
-    try{ fs.unlink(filePath); }catch{}
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve);
+      writer.on('error', reject);
+    });
+  }).then(() => {
+	
+	let buttonMessage = {
+                        document: fs.readFileSync(filePath),
+                        mimetype: 'application/vnd.android.package-archive',
+                        fileName: data.name+`.apk`,
+                        caption : inf
+                        
+                    }
+                  Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
+
+    console.log('File downloaded successfully');
+
+  
+    fs.unlink(filePath, (err) => {
+      if (err) { console.error('Error deleting file:', err); } else { console.log('File deleted successfully'); } });
   }) .catch(error => {
-	try{ fs.unlink(filePath); }catch{}
-    return citel.reply('*_Apk not Found, Sorry_*')//:', error.message);
+	fs.unlink(filePath)
+    return citel.reply('*Apk not Found, Sorry*')//:', error.message);
   });
-	})
+/}
+)
 //-------------------------------------------------------------------------------
 cmd({
             pattern: "song",
