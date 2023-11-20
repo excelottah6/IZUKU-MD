1134b6f4cb807cf90647a18e85762fcc3402c3bb
ɪɴɴᴏxᴇɴᴛ-ᴢᴜʙɪ <143280418+Maher-Zubair@users.noreply.github.com>
2023-11-20 16:01:27 +0500

    LOGO IDEA

--- commands/logo.js
+++ commands/logo.js
@@ -3,12 +3,12 @@
  const Config = require('../config')
  const prefix = Config.prefix
  const maker = require('mumaker')
- 
+ let cap = `*_Made By ${Config.botname}_*`
      //---------------------------------------------------------------------------
  cmd({ pattern: "deepsea", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
          if (!text) return citel.reply('_Need text._')
          let anu = await maker.textpro('https://textpro.me/create-3d-deep-sea-metal-text-effect-online-1053.html', text)
-         Void.sendMessage(citel.chat, { image: { url: anu.image }, caption: `Made by ${tlang().title},For my ${tlang().greet}` }, { quoted: citel })
+         Void.sendMessage(citel.chat, { image: { url: anu.image }, caption: cap }, { quoted: citel })
      })
      //---------------------------------------------------------------------------
  cmd({ pattern: "horror", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
