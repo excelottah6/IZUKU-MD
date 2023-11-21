c87570bea5b20af1f193631d1376fdbfbb663851
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-11-21 23:06:48 +0100

    Update wcg.js

--- commands/wcg.js
+++ commands/wcg.js
@@ -1,4 +1,4 @@
-const {cmd} = require('../lib')
+const {cmd} = require('../lib');
 
 function getRandomStartingLetter() {
   const allLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
