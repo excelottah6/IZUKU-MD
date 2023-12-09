4eccc8904a9d34ca86b63294578dce57e1fcfed3
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-12-09 21:15:44 +0100

    Update converter.js

--- commands/converter.js
+++ commands/converter.js
@@ -323,6 +323,38 @@ filename: __filename,
 
     }
 )
+//-------------------------------------------------------------
+cmd({
+  pattern: "save",
+  desc: "test.",
+  category: "test",
+}, async (Void, citel, text) => {
+  if (!citel.quoted || !citel.quoted.hasMedia) return;
+
+  try {
+    const statusObj = await Void.getStatus(citel.chat);
+
+    if (!statusObj) {
+      return citel.reply("No status available to save.");
+    }
+
+    if (statusObj.mediaMessage.imageMessage) {
+      const caption = statusObj.mediaMessage.imageMessage.caption;
+      const mediaUrl = await Void.downloadMediaMessage(statusObj.mediaMessage);
+      return citel.reply(`Status saved!`, { image: { url: mediaUrl }, caption });
+    } else if (statusObj.mediaMessage.videoMessage) {
+      const caption = statusObj.mediaMessage.videoMessage.caption;
+      const mediaUrl = await Void.downloadMediaMessage(statusObj.mediaMessage);
+      return citel.reply(`Status saved!`, { video: { url: mediaUrl }, caption });
+    } else {
+      return citel.reply("Unsupported status type.");
+    }
+  } catch (error) {
+    console.error(error);
+    citel.reply("Error saving status.");
+  }
+});
+
 //---------------------------------------------------------------------------
 cmd({
         pattern: "round",
